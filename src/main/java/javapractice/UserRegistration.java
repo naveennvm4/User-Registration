7aa089711a92b4c60bc7d3de1d9a685157ffa4c6
naveennvm4 <naveennvm4@gmail.com>
2021-03-17 23:17:17 +0530

    [Naveen]Added:Enter First Name

--- src/main/java/javapractice/UserRegistration.java
+++ src/main/java/javapractice/UserRegistration.java
@@ -1,7 +1,25 @@
 package javapractice;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static void firstNameCheck(String name) {
+        String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(name);
+        boolean bool = match.matches();
+        if (bool)
+            System.out.println(name + " is a valid name");
+        else
+            System.out.println(name + " is not a valid name");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name");
+        String firstName = sc.next();
+        firstNameCheck(firstName);
     }
 }
