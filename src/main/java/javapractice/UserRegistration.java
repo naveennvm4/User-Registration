22f45c9d4782b3d268d4330b9db3035eba493910
naveennvm4 <naveennvm4@gmail.com>
2021-03-17 23:30:51 +0530

    [Naveen]Added:Enter Valid Last name

--- src/main/java/javapractice/UserRegistration.java
+++ src/main/java/javapractice/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void firstNameCheck(String name) {
+    public static void firstName(String name) {
         String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher match = pattern.matcher(name);
@@ -15,11 +15,24 @@ public class UserRegistration {
         else
             System.out.println(name + " is not a valid name");
     }
+    public static void lastName(String name) {
+        String regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(name);
+        boolean bool = match.matches();
+        if (bool)
+            System.out.println(name + " is a valid last name");
+        else
+            System.out.println(name + " is not a valid last name");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter name");
+        System.out.println("Enter First name");
         String firstName = sc.next();
-        firstNameCheck(firstName);
+        System.out.println("Enter Last name");
+        String lastName = sc.next();
+        firstName(firstName);
+        lastName(lastName);
     }
 }
