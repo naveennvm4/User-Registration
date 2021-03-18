dc8771b4c4c53ee23c9263087e66c748f1d91823
naveennvm4 <naveennvm4@gmail.com>
2021-03-18 07:16:47 +0530

    [Naveen]Rule 1:Enter Password

--- src/main/java/javapractice/UserRegistration.java
+++ src/main/java/javapractice/UserRegistration.java
@@ -5,12 +5,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static Scanner sc = new Scanner(System.in);
-    public static void entry(String firstName, String lastName, String email, String mobile) {
+    public static void entry(String firstName, String lastName, String email, String mobile, String password) {
         System.out.println("First Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName));
         System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName));
         System.out.println("E-Mail: " + Pattern.matches("^[a-zA-Z0-9]{3,}([.]{1}+[a-zA-Z0-9]{3,})*[@]" +
                 "           {1}[a-zA-Z0-9]{2,}[.]{1}[a-zA-Z0-9]{2,}([.]{1}[a-zA-Z0-9]{2,})*$", email));
         System.out.println("Mobile Number: " + Pattern.matches("^[9][1][\s][6-9][0-9]{9}$", mobile));
+        System.out.println("Password: " + Pattern.matches("^[a-zA-Z0-9]{8,}$", password));
     }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
@@ -22,6 +23,8 @@ public class UserRegistration {
         String email = sc.nextLine();
         System.out.println("Enter Mobile No");
         String mobile = sc.nextLine();
-        entry(firstName, lastName, email, mobile);
+        System.out.println("Enter password");
+        String password = sc.nextLine();
+        entry(firstName, lastName, email, mobile, password);
     }
 }
