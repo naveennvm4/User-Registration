8def19e47d4ca401564659bd65e32aec951c5102
naveennvm4 <naveennvm4@gmail.com>
2021-03-18 10:55:34 +0530

    [Naveen]REFACTORED:Lambda fuction to validate User entry

--- src/main/java/javapractice/UserRegistration.java
+++ src/main/java/javapractice/UserRegistration.java
@@ -1,81 +1,37 @@
 package javapractice;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
+interface UserEntry {
+    boolean validityCheck(String pattern, String name);
+}
 
 public class UserRegistration {
-    public static boolean isFirstName(String firstName) throws UserRegistrationException {
-        if(Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", firstName))
-            return true;
-        else
-            throw new UserRegistrationException("Invalid First Name");
-    }
-
-    public static boolean isLastName(String lastName) throws UserRegistrationException {
-        if(Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", lastName))
-            return true;
-        else
-            throw new UserRegistrationException("Invalid Last Name");
-    }
-
-    public static boolean isEmail(String email) throws UserRegistrationException {
-        if(Pattern.matches("^[a-zA-Z0-9]{3,}([\\.\\+\\-]?[a-zA-Z0-9]{3,})?[@][A-Za-z0-9]{1,}[.]" +
-                            "[A-Za-z]{2,4}[,]?([.][A-Za-z]{2,4}[.]?)?$", email))
-            return true;
-        else
-            throw new UserRegistrationException("Invalid Email");
-
-    }
-
-    public static boolean isMobileNumber(String mobile) throws UserRegistrationException {
-        if(Pattern.matches("^[9][1][\\s][6-9][0-9]{9}$", mobile))
-            return true;
-        else
-            throw new UserRegistrationException("Invalid Mobile Number");
-    }
-
-    public static boolean isPassword(String password) throws UserRegistrationException {
-        if(Pattern.matches("^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9])" +
-                            "(?=.*[a-z]).{8,}$", password))
-            return true;
-        else
-            throw new UserRegistrationException("Invalid Password");
-    }
+    static String FIRST_NAME = "^[A-Z]{1}[a-zA-Z]{2,}$";
+    static String LAST_NAME = "^[A-Z]{1}[a-zA-Z]{2,}$";
+    static String EMAIL = "^[a-zA-Z0-9]{3,}([\\.\\+\\-]?[a-zA-Z0-9]{3,})?[@][A-Za-z0-9]{1,}[.]" +
+                                    "[A-Za-z]{2,4}[,]?([.][A-Za-z]{2,4}[.]?)?$";
+    static String MOBILE_NUMBER = "^[9][1]\\s[6-9][0-9]{9}$";
+    static String PASSWORD = "^(?=.*[\\@\\#\\$\\%\\&\\_\\,\\.])(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$";
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String firstName = "Naveen";
-        String lastName = "Nani";
-        String email = "naveennvm4@gmail.com";
-        String mobile = "91 7095252907";
-        String password = "Naveen@216";
-        UserRegistration person = new UserRegistration();
-        try {
-            isFirstName(firstName);
-        }
-        catch(Exception e) {System.out.println("Exception is " + e);
-        }
-        try {
-            isLastName(lastName);
-        }
-        catch(Exception e) {
-            System.out.println("Exception is " + e);
-        }
-        try {
-            isEmail(email);
-        }
-        catch(Exception e) {
-            System.out.println("Exception is " + e);
-        }
-        try {
-            isMobileNumber(mobile);
-        }
-        catch(Exception e) {
-            System.out.println("Exception is " + e);
-        }
-        try {
-            isPassword(password);
-        }
-        catch(Exception e) {
-            System.out.println("Exception is " + e);
-        }
+        UserEntry validate = (String pattern, String name) -> Pattern.matches(pattern, name);
+        System.out.println("Enter First Name");
+        String firstName = sc.next();
+        System.out.println("Enter Last Name");
+        String lastName = sc.next();
+        System.out.println("Enter email");
+        String email = sc.next();
+        System.out.println("Enter Mobile Number");
+        String mobile = sc.next();
+        System.out.println("Enter Password");
+        String password = sc.next();
+
+        System.out.println("Entered First Name is " + validate.validityCheck(FIRST_NAME, firstName));
+        System.out.println("Entered Last Name is " + validate.validityCheck(LAST_NAME, lastName));
+        System.out.println("Entered Email is " + validate.validityCheck(EMAIL, email));
+        System.out.println("Entered Mobile Number is " + validate.validityCheck(MOBILE_NUMBER, mobile));
+        System.out.println("Entered Password is " + validate.validityCheck(PASSWORD, password));
     }
 }
